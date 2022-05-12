86926c5cc2c7baf67ff7e6b70a0629a1b8cc46e8
ZSamuels28 <ZSamuels28@gmail.com>
2022-05-12 09:16:02 -0700

    Added time tracking

--- scripts/recipvalidate.js
+++ scripts/recipvalidate.js
@@ -3,8 +3,9 @@ const axiosRetry = require("axios-retry");
 const csv = require("fast-csv");
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 const dotenv = require("dotenv");
-const path = require('path')
-dotenv.config({ path: path.resolve(__dirname, '../config/variables.env') });
+const path = require("path");
+const { JSDOM } = require("jsdom");
+dotenv.config({ path: path.resolve(__dirname, "../config/variables.env") });
 const myArgs = process.argv.slice(2);
 const SPARKPOST_API_KEY = process.env.SPARKPOST_API_KEY;
 const SPARKPOST_HOST = process.env.SPARKPOST_HOST;
@@ -28,71 +29,75 @@ const errorWriter = createCsvWriter({
 });
 
 async function getRecipientValidation(
-    GoodEmailList,
-    BadEmailList,
-    BadEmailCount,
-    GoodEmailCount
-  ) {
-    axiosRetry(axios, {
-      retries: 3, // number of retries
-      retryDelay: (retryCount) => {
-        console.log(`retry attempt: ${retryCount}`);
-        return retryCount * 20000; // time interval between retries
-      },
-      retryCondition: (error) => {
-        // if retry condition is not specified, by default idempotent requests are retried
-        return error.response.status === 503;
-      },
-    });
-  
-    let completed = 0;
-    try {
-      const promises = GoodEmailList.map(async (email) => {
-        const response = await axios({
-          method: "GET",
-          url: SPARKPOST_HOST + "/api/v1/recipient-validation/single/" + email,
-          headers: {
-            Authorization: SPARKPOST_API_KEY,
-          },
-        }).catch((error) => {
-          console.log("Error " + error.response.status + " - " + error);
-        });
-  
-        completed++;
-        process.stdout.write(
-          "Done with " + completed + " / " + GoodEmailList.length + "\r"
-        );
-  
-        return {
-          email: email,
-          valid: response.data.results.valid,
-          result: response.data.results.result,
-          is_role: response.data.results.is_role,
-          is_disposable: response.data.results.is_disposable,
-          is_free: response.data.results.is_free,
-          delivery_confidence: response.data.results.delivery_confidence,
-        };
+  GoodEmailList,
+  BadEmailList,
+  BadEmailCount,
+  GoodEmailCount
+) {
+  axiosRetry(axios, {
+    retries: 3, // number of retries
+    retryDelay: (retryCount) => {
+      console.log(`retry attempt: ${retryCount}`);
+      return retryCount * 20000; // time interval between retries
+    },
+    retryCondition: (error) => {
+      // if retry condition is not specified, by default idempotent requests are retried
+      return error.response.status === 503;
+    },
+  });
+
+  const { window } = new JSDOM();
+  const start = window.performance.now();
+  let completed = 0;
+
+  try {
+    const promises = GoodEmailList.map(async (email) => {
+      const response = await axios({
+        method: "GET",
+        url: SPARKPOST_HOST + "/api/v1/recipient-validation/single/" + email,
+        headers: {
+          Authorization: SPARKPOST_API_KEY,
+        },
+      }).catch((error) => {
+        console.log(`Error ${error.response.status} - ${error}`);
       });
-  
-      const results = await Promise.all(promises);
-      csvWriter.writeRecords(results);
-      if (BadEmailCount == 0) {
-        console.log("All emails successfully validated.");
-      } else {
-        console.log(
-          "Email validation completed with the following results:\n\x1b[32mEmails Successfully Validated: " +
-            GoodEmailCount +
-            "\n\x1b[31mEmails with Errors: " +
-            BadEmailCount +
-            " (See logs/errors.csv for invalid emails)"
-        );
-        errorWriter.writeRecords(BadEmailList);
-      }
-    } catch (err) {
-      /**
-       * If the request is rejected, then the catch method will be executed.
-       */
+
+      completed++;
+      process.stdout.write(
+        `Done with ${completed} / ${GoodEmailList.length} \r`
+      );
+
+      return {
+        email: email,
+        valid: response.data.results.valid,
+        result: response.data.results.result,
+        is_role: response.data.results.is_role,
+        is_disposable: response.data.results.is_disposable,
+        is_free: response.data.results.is_free,
+        delivery_confidence: response.data.results.delivery_confidence,
+      };
+    });
+
+    const results = await Promise.all(promises);
+    const stop = window.performance.now();
+    csvWriter.writeRecords(results);
+    if (BadEmailCount == 0) {
+      console.log(
+        `All emails successfully validated in ${(stop - start) / 1000} seconds`
+      );
+    } else {
+      console.log(
+        `Email validation completed in ${
+          (stop - start) / 1000
+        } seconds with the following results:\n\x1b[32mEmails Successfully Validated: ${GoodEmailCount}\n\x1b[31mEmails with Errors: ${BadEmailCount} (See logs/errors.csv for invalid emails)`
+      );
+      errorWriter.writeRecords(BadEmailList);
     }
+  } catch (err) {
+    /**
+     * If the request is rejected, then the catch method will be executed.
+     */
   }
+}
 
 module.exports = { getRecipientValidation };
